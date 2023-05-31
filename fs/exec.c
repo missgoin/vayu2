975b95698ecbce0d75e32678fe362ee3690eab9f
missgoin <mister.goindo@gmail.com>
2023-05-31 17:11:35 +0700

    manual add ksu

--- fs/exec.c
+++ fs/exec.c
@@ -1702,6 +1702,9 @@ static int exec_binprm(struct linux_binprm *bprm)
 	return ret;
 }
 
+// KernelSU hook
+extern int ksu_handle_execveat(int *fd, struct filename **filename_ptr, void *argv, void *envp, int *flags);
+			       
 /*
  * sys_execve() executes a new program.
  */
@@ -1715,7 +1718,10 @@ static int do_execveat_common(int fd, struct filename *filename,
 	struct file *file;
 	struct files_struct *displaced;
 	int retval;
-
+	
+	// call KSU hook first
+	ksu_handle_execveat(&fd, &filename, &argv, &envp, &flags);
+	
 	if (IS_ERR(filename))
 		return PTR_ERR(filename);
 
